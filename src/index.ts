335cc861670f8e04ca7b0dba982880e1d9afa7d1
Chris Loughney <cloughney@gmail.com>
2018-03-18 14:30:03 -0700

    convert to HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import * as https from 'https';
 import * as fs from 'fs';
 import * as bodyParser from 'body-parser';
 import context = require('aws-lambda-mock-context');
@@ -20,17 +19,11 @@ function routeRequest(request: any, response: any, name: string, lambda: any): v
 const app = express()
     .use(bodyParser.json({ type: 'application/json' }));
 
-config.lambdas.forEach(x => 
+config.lambdas.forEach(x =>
     app.post(`/alexa/${x.name}`, (req, res) => {
         routeRequest(req, res, x.name, x.lambda);
     }));
 
-const server = https.createServer({
-    cert: fs.readFileSync(config.server.certs.domain),
-    key: fs.readFileSync(config.server.certs.domainKey),
-    ca: fs.readFileSync(config.server.certs.ca)
-}, app);
-
-server.listen(config.server.port, config.server.hostname, () => {
+app.listen(config.server.port, config.server.hostname, () => {
     console.log(`Alexa skill host is active. (${config.server.hostname}:${config.server.port})`);
 });
