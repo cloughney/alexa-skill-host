4ae0d77d7a0eb4b5a4a930f88d95fd32ccb4d89b
Chris Loughney <cloughney@gmail.com>
2018-10-12 22:52:48 -0700

    update alexa skill packages, remove media-center skill, create couch skill

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 exports.default = {
-
     server: {
         hostname: '0.0.0.0',
         port: 8081
     },
-
     lambdas: [
         { name: "media-center", lambda: require('./dist/media-center/lambda') },
         { name: "unicorn", lambda: require('./dist/unicorn/lambda') },
-        { name: "phone", lambda: require('./dist/phone/lambda') }
+        { name: "couch", lambda: require('./dist/couch/lambda') }
     ]
-
 };
---
+++ src/couch/lambda.ts
@@ -0,0 +1,100 @@
+import * as Alexa from 'ask-sdk-core';
+import * as http from 'http';
+
+const colors: { [color: string ]: { r: number, g: number, b: number } } = {
+    'red': { r: 255, g: 0, b: 0 },
+    'green': { r: 0, g: 255, b: 0 },
+    'blue': { r: 0, g: 0, b: 255 }
+};
+
+async function sendRequest(path: string, body?: any): Promise<void> {
+    const options = {
+        method: 'POST', 
+        hostname: '192.168.1.174', port: 80,
+        path, body, rejectUnauthorized: false,
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    return new Promise<void>((resolve, reject) => {
+        http.request(options, () => resolve())
+            .on('error', err => reject(err))
+            .end();
+    });
+}
+
+const SetColorHandler: Alexa.RequestHandler = {
+    canHandle(input) {
+        const request = input.requestEnvelope.request;
+        // request.type === 'LaunchRequest' ?
+        return request.type === 'IntentRequest' && request.intent.name === 'SetColor';
+    },
+    async handle(input) {
+        console.log('Handling SetColor intent');
+
+        const { request } = input.requestEnvelope as any; //TODO why do I need to cast?
+        const { colorInput } = request.intent.slots;
+        if (!colorInput.value || colorInput.value === '?') {
+            return input.responseBuilder
+                .speak("I don't know what's going on.")
+                .getResponse();
+        }
+
+        const color = colors[colorInput.value];
+        if (!color) {
+            return input.responseBuilder
+                .speak("I don't know how to make that color.")
+                .getResponse();
+        }
+
+        try {
+            await sendRequest('/api/lights/1', { mode: 'color', color });
+        } catch (err) {
+            console.dir(err);
+            return input.responseBuilder
+                .speak('An error occurred while contacting the device.')
+                .getResponse();
+        }
+
+        return input.responseBuilder
+            .speak('Okay')
+            .getResponse();
+    }
+}
+
+const TurnOffHandler: Alexa.RequestHandler = {
+    canHandle(input) {
+        const { request } = input.requestEnvelope;
+        return request.type === 'IntentRequest' && request.intent.name === 'TurnOff';
+    },
+    async handle(input) {
+        console.log('Handling TurnOff intent');
+
+        try {
+            await sendRequest('/api/lights/1', { mode: 'off' });
+        } catch (err) {
+            console.dir(err);
+            return input.responseBuilder
+                .speak('An error occurred while contacting the device.')
+                .getResponse();
+        }
+
+        return input.responseBuilder
+            .speak('Okay')
+            .getResponse();
+    }
+}
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        SetColorHandler,
+        TurnOffHandler
+    )
+    .lambda();
+
+// function (event: Alexa.RequestBody<Alexa.Request>, context: Alexa.Context) {
+//     const alexa = Alexa.handler(event, context);
+//     alexa.registerHandlers(handlers);
+//     alexa.execute();
+// };
