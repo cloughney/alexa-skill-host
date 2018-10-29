03fbfda96823ec8e0c055d5dd4a0a481ba85bf52
Chris Loughney <me@chrisloughney.com>
2018-10-29 06:09:30 +0000

    update flags enum

--- src/data.ts
+++ src/data.ts
@@ -4,8 +4,8 @@ export enum DeviceType {
 
 export enum DeviceCapabilities {
     Brightness = 0,
-    Color = 0 << 0,
-    Power = 0 << 1
+    Color = 1 << 0,
+    Power = 1 << 1
 }
 
 export const devices = [
@@ -13,18 +13,21 @@ export const devices = [
         id: 'couch-bottom-left',
         type: DeviceType.Light,
         capabilities: DeviceCapabilities.Power | DeviceCapabilities.Color | DeviceCapabilities.Brightness,
+        name: 'couch bottom left',
         endpoint: 'http://192.168.1.115/api/lights/1'
     },
     {
         id: 'couch-bottom-middle',
         type: DeviceType.Light,
         capabilities: DeviceCapabilities.Power | DeviceCapabilities.Color | DeviceCapabilities.Brightness,
+        name: 'couch bottom middle',
         endpoint: 'http://192.168.1.115/api/lights/2'
     },
     {
         id: 'couch-bottom-right',
         type: DeviceType.Light,
         capabilities: DeviceCapabilities.Power | DeviceCapabilities.Color | DeviceCapabilities.Brightness,
+        name: 'couch bottom right',
         endpoint: 'http://192.168.1.115/api/lights/3'
     }
 ]
